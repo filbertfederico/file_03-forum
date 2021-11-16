09f2a36eba9cacb0138ea6aa0f51de7e629c2b37
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-11-16 10:48:42 +0700

    Update SentenceSplitter.py

--- File03 - files/SentenceSplitter.py
+++ File03 - files/SentenceSplitter.py
@@ -1,11 +1,11 @@
 import re
 
-def spliteroo():
+def split():
     text = ("Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't.")
     output = re.split(r"(?<=[^A-Z].[.?]) +(?=[A-Z])", text)
     for i in output :
         print(i)
-spliteroo()
+split()
 
 
 #
