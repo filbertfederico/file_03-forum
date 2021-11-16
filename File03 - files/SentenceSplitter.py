24e2ee0ddb21d2065a5c1f44f455007bb28574da
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-11-16 10:53:13 +0700

    Update SentenceSplitter.py

--- File03 - files/SentenceSplitter.py
+++ File03 - files/SentenceSplitter.py
@@ -3,8 +3,8 @@ import re
 def split():
     text = ("Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't.")
     output = re.split(r"(?<=[^A-Z].[.?]) +(?=[A-Z])", text)
-    for i in output :
-        print(i)
+    for g in output :
+        print(g)
 split()
 
 
